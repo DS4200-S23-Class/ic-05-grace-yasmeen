23dcb649a60b3c3b659ea10ddfa7fd838e3caf70
Grace Kelner <gracekelner@Graces-MacBook-Pro.local>
2023-01-30 18:47:17 -0500

    Update

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function buttonClicked()
 	let buttonDiv = document.getElementById("button-div");
 
 	
-	buttonDiv.innerHTML = count;
+	buttonDiv.innerHTML = "Number of times button clicked:" + count;
 
 }
 
